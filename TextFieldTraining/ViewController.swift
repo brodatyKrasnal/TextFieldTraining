2acd141e2dc8981c7f79fb26ca9ab0797b468aeb
detymek <tymon.gf@hotmail.com>
2020-04-09 13:08:23 +0200

    Initial Commit

---
+++ TextFieldTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldTraining
+//
+//  Created by Daniel Golęba Frygies on 09/04:100.
+//  Copyright © 2020 Tymon Golęba Frygies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
