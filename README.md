2561b849cbdbc4c56c4f2d8ffb7f2f963df49262
detymek <tymon.gf@hotmail.com>
2020-04-09 13:57:56 +0200

    UITextField and delegate training

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+#  TextField Training app
+
+## Purpose
+
+This app is to serve as a training space for UITextField & UITextFieldDelegate objects.
+
+
+## What you will create
+
+By the end of the module, you will have made a beautiful, dark-mode enabled weather app. You'll be able to check the weather for the current location based on the GPS data from the iPhone as well as by searching for a city manually. 
+
+## What have I gained?
--- TextFieldTraining/ViewController.swift
+++ TextFieldTraining/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var contactNumberField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        passwordField.delegate = self
+        usernameField.delegate = self
+        contactNumberField.delegate = self
     }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//    }
 
 
+    @IBAction func buttonAction(_ sender: Any) {
+        textView.text = "Username: \(String(describing: usernameField.text)) \n Passowrd: \(passwordField.text!) \n Contact number: \(contactNumberField.text!)"
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //when touched outside the contactNumberField keyboard will be dismissed
+        contactNumberField.resignFirstResponder()
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+      // when you tap the field it will resign the first responder status
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
